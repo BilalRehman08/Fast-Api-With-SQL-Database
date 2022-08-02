11f4d94d4de458fc550c2462ef7335a835c043ae
BilalRehman08 <bilalrehman080808@gmail.com>
2022-08-02 18:11:59 +0500

    setup database in main.py

--- main.py
+++ main.py
@@ -1,77 +1,15 @@
-# main.py
-from re import A
-from fastapi import FastAPI, Body
-import schemas
-app = FastAPI()
+from fastapi import FastAPI
 
-fakeDatabase = {
-    1: {'task': 'Learn Neo4j'},
-    2: {'task': 'Learn Flutter'},
-    3: {'task': 'learm Fast Api'}
-}
+from database import Base, engine, SessionLocal
+from sqlalchemy.orm import Session
 
-# ==================================== GET ====================================
-
-# Get All records
+# This will create our database if it doesent already exists
+Base.metadata.create_all(engine)
 
 
-@app.get('/')
-def get_all_items():
-    return fakeDatabase
-
-# Get single record with specific id
-
-
-@app.get('/{id}')
-def get_single_item(id: int):
-    return fakeDatabase[id]
-
-
-# ==================================== POST ====================================
-
-# Adding a record in existing fakeDatabase (Option # 1 | Query Parameter)
-
-
-@app.post('/addItemThroughQueryParamter')
-def add_item_through_query_parameter(task: str):
-    newId = len(fakeDatabase)+1
-    fakeDatabase[newId] = {'task': task}
-    return fakeDatabase
-
-# Adding a record in existing fakeDatabase (Option # 2 | Request Body)
-
-
-@app.post('/addItemThroughRequestBody')
-def add_item_through_request_body(item: schemas.Item):
-    newId = len(fakeDatabase)+1
-    fakeDatabase[newId] = {'task': item.task}
-    return fakeDatabase
-
-# Adding a record in existing fakeDatabase (Option # 3 | Request Body as a Dictionary)
-
-
-@app.post('/addItemThroughRequestBodyAsDictionary')
-def add_item_through_request_body_as_dictionary(body=Body()):
-    newId = len(fakeDatabase)+1
-    fakeDatabase[newId] = {"task": body}
-    return fakeDatabase
-
-# ==================================== UPDATE ====================================
-
-# updating single record
-
-
-@app.put("/{id}")
-def update_task(id: int, item: schemas.Item):
-    fakeDatabase[id]['task'] = item.task
-    return fakeDatabase
-
-# ==================================== DELETE ====================================
-
-# deleting record using id
-
-
-@app.delete("/{id}")
-def delete_record(id: int):
-    del fakeDatabase[id]
-    return fakeDatabase
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
