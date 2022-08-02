381ae8f6507b7fbb6955548049ba970a595af4cb
BilalRehman08 <bilalrehman080808@gmail.com>
2022-08-02 18:08:22 +0500

    database setup

---
+++ database.py
@@ -0,0 +1,6 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+engine = create_engine("sqlite:///todo.db")
+Base = declarative_base()
+SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
