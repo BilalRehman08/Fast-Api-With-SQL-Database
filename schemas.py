c46ce6c487e295bcc23fb6f8d0395d0e184b82bc
BilalRehman08 <bilalrehman080808@gmail.com>
2022-08-03 12:16:16 +0500

    changes in schemas.py

--- schemas.py
+++ schemas.py
@@ -1,6 +1,14 @@
-import imp
+from datetime import date
 from pydantic import BaseModel
 
 
-class Item(BaseModel):
-    task: str
+class Record(BaseModel):
+    id: int
+    date: date
+    country: str
+    cases: int
+    deaths: int
+    recoveries: int
+
+    class Config:
+        orm_mode = True
