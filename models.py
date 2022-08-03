00cf2e97c986c7212297fa96783aaaa36fd00aa8
BilalRehman08 <bilalrehman080808@gmail.com>
2022-08-03 12:15:01 +0500

    changes in models.py

--- models.py
+++ models.py
@@ -1,8 +1,15 @@
+
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.types import Date
 from database import Base
 
 
-class Item(Base):
-    __tablename__ = 'items'
-    id = Column(Integer, primary_key=True)
-    task = Column(String(256))
+class Record(Base):
+    __tablename__ = "Records"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(Date)
+    country = Column(String(255), index=True)
+    cases = Column(Integer)
+    deaths = Column(Integer)
+    recoveries = Column(Integer)
